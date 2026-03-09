9bfefc57f1edb8d43aba85effe7224bfa1a5157f
anton-soenderskov <antonjimmyheino@gmail.com>
2026-03-09 19:13:03 +0100

    added other functionality to the program

--- src/main.c
+++ src/main.c
@@ -11,24 +11,35 @@ void print_usage(char *argv[]) {
     printf("Usage: %s -n -f <database file>\n", argv[0]);
     printf("\t -n - create new database file\n");
     printf("\t -f - (required) path to database file\n");
+    printf("\t -l - list the employees\n");
+    printf("\t -a - add via CSV lsit of (name,address,salary)\n");
     return;
 }
 
 int main(int argc, char *argv[]) { 
     char *filepath = NULL;
+    char *addstring = NULL;
     bool newfile = false;
+    bool list = false;
     int c;
 
     int dbfd = -1;
     struct dbheader_t *dbhdr = NULL;
+    struct employee_t *employees = NULL;
 
-	while ((c = getopt(argc, argv, "nf:")) != -1) {
+	while ((c = getopt(argc, argv, "nf:a:l")) != -1) {
         switch (c) {
             case 'n':
                 newfile = true;
                 break;
             case 'f':
                 filepath = optarg;
+                break;
+            case 'a':
+                addstring = optarg;
+                break;
+            case 'l':
+                list = true;
                 break;
             case '?':
                 printf("Unknown option -%c\n", c);
@@ -67,9 +78,21 @@ int main(int argc, char *argv[]) {
         }
     }
 
-    printf("Newfile: %d\n", newfile);
-    printf("Filepath: %s\n", filepath);
+    if (read_employees(dbfd, dbhdr, &employees) != STATUS_SUCCESS) {
+        printf("Failed to read employees\n");
+        return 0;
+    }
 
-    output_file(dbfd, dbhdr);
+    if (addstring) {
+        add_employee(dbhdr, &employees, addstring);
+    }
+    
+    if (list) {
+        list_employees(dbhdr, employees);
+    }
+
+    output_file(dbfd, dbhdr, employees);
+
+
     return 0;
 }
--- src/parse.c
+++ src/parse.c
@@ -11,34 +11,92 @@
 #include "parse.h"
 
 void list_employees(struct dbheader_t *dbhdr, struct employee_t *employees) {
-
+    int i = 0;
+    for (; i < dbhdr->count; i++) {
+        printf("Employee %d\n", i);
+        printf("\tName: %s\n", employees[i].name);
+        printf("\tAddress: %s\n", employees[i].address);
+        printf("\tHours: %d\n", employees[i].hours);
+    }
 }
 
-int add_employee(struct dbheader_t *dbhdr, struct employee_t *employees, char *addstring) {
-    return 0;
+int add_employee(struct dbheader_t *dbhdr, struct employee_t **employees, char *addstring) {
+
+    if (NULL == dbhdr) return STATUS_ERROR;
+    if (NULL == employees) return STATUS_ERROR;
+    if (NULL == *employees) return STATUS_ERROR;
+    if (NULL == addstring) return STATUS_ERROR;
+
+    char *name = strtok(addstring, ",");
+    if (name == NULL) return STATUS_ERROR;
+    char *addr = strtok(NULL, ",");
+    if (addr == NULL) return STATUS_ERROR;
+    char *hours = strtok(NULL, ",");
+    if (hours == NULL) return STATUS_ERROR;
+
+    struct employee_t *e = *employees;
+    e = realloc(e, sizeof(struct employee_t)*dbhdr->count+1);
+    if (e == NULL) {
+        return STATUS_ERROR;
+    }
+
+    dbhdr->count++;
+
+    strncpy(e[dbhdr->count-1].name, name, sizeof(e[dbhdr->count-1].name)-1);
+    strncpy(e[dbhdr->count-1].address, addr, sizeof(e[dbhdr->count-1].address)-1);
+    e[dbhdr->count-1].hours = atoi(hours);
+
+    *employees = e;
+
+    return STATUS_SUCCESS;
 }
 
 int read_employees(int fd, struct dbheader_t *dbhdr, struct employee_t **employeesOut) {
-    return 0;
-}
-
-// int output_file(int fd, struct dbheader_t *dbhdr, struct employee_t *employees) {
-//     return 0;
-// }	
-
-void output_file(int fd, struct dbheader_t *dbhdr) {
     if (fd < 0) {
         printf("Invalid file descriptor\n");
-        return;
+        return STATUS_ERROR;
     }
 
+    int count = dbhdr->count;
+
+    struct employee_t *employees = calloc(count, sizeof(struct employee_t));
+    if (employees == NULL) {
+        printf("Calloc failed\n");
+        return STATUS_ERROR;
+    }
+
+    read(fd, employees, count*sizeof(struct employee_t));
+
+    int i = 0;
+    for (; i<count; i++) {
+        employees[0].hours = ntohl(employees[i].hours);
+    }
+    *employeesOut = employees;
+    return STATUS_SUCCESS;
+}	
+
+int output_file(int fd, struct dbheader_t *dbhdr, struct employee_t *employees) {
+    if (fd < 0) {
+        printf("Invalid file descriptor\n");
+        return STATUS_ERROR;
+    }
+
+    int realcount = dbhdr->count;
+
     dbhdr->magic = htonl(dbhdr->magic);
-    dbhdr->filesize = htonl(dbhdr->filesize);
+    dbhdr->filesize = htonl(sizeof(struct dbheader_t)+(sizeof(struct employee_t)*realcount));
     dbhdr->count = htons(dbhdr->count);
     dbhdr->version = htons(dbhdr->version);
 
     lseek(fd, 0, SEEK_SET);
     write(fd, dbhdr, sizeof(struct dbheader_t));
+
+    int i = 0;
+    for(; i<realcount; i++) {
+        employees[i].hours = htonl(employees[i].hours);
+        write(fd, &employees[i], sizeof(struct employee_t));
+    }
+    return STATUS_SUCCESS;
 }
 
 int validate_db_header(int fd, struct dbheader_t **headerOut) {
