cc6c5117efdc59cc275fc72806b69480ac05a2b2
anton-soenderskov <antonjimmyheino@gmail.com>
2026-03-09 19:21:41 +0100

    fixed reallocation of e in add_employee function

--- src/parse.c
+++ src/parse.c
@@ -35,7 +35,7 @@ int add_employee(struct dbheader_t *dbhdr, struct employee_t **employees, char *
     if (hours == NULL) return STATUS_ERROR;
 
     struct employee_t *e = *employees;
-    e = realloc(e, sizeof(struct employee_t)*dbhdr->count+1);
+    e = realloc(e, sizeof(struct employee_t) * (dbhdr->count + 1));
     if (e == NULL) {
         return STATUS_ERROR;
     }
